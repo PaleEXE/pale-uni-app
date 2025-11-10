80bdfbed0e598cce338afe9529e6ca9d7bd8e2a7
PaleEXE <150634604+PaleEXE@users.noreply.github.com>
2025-11-10 17:41:56 +0300

    even better ui 2

---
+++ src/app/services/data.service.ts
@@ -0,0 +1,23 @@
+export const topics = [
+  {
+    title: 'Knowledge Representation',
+    description:
+      'Knowledge representation (KR) aims to model information in a structured manner to formally represent it as knowledge in knowledge-based systems whereas knowledge representation and reasoning (KRR, KR&R, or KR²) also aims to understand, reason, and interpret knowledge.',
+    subtopics: ['Logic Evaluator'],
+  },
+  {
+    title: 'Data Mining',
+    description:
+      'Data Mining and ClusteringData mining is the process of extracting and finding patterns in massive data sets involving methods at the intersection of machine learning, statistics, and database systems. Data mining is an interdisciplinary subfield of computer science and statistics with an overall goal of extracting information (with intelligent methods) from a data set and transforming the information into a comprehensible structure for further use.',
+    subtopics: ['Clustring', 'FP-Growth'],
+  },
+  { title: 'I D K', subtopics: [] },
+  { title: 'Shaw', description: 'GIT GUD', subtopics: ['sui'] },
+];
+
+export function slugify(text: string) {
+  return text
+    .toLowerCase()
+    .replace(/\s+/g, '-')
+    .replace(/[^\w-]+/g, '');
+}
